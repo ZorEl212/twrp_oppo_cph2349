b31bdb327425d7e6dcc3644923bbe3ec1554f295
ZorEl212 <93014559+ZorEl212@users.noreply.github.com>
2022-07-05 03:57:57 +0100

    fix 64 bit support

--- twrp_cph2349.mk
+++ twrp_cph2349.mk
@@ -18,6 +18,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
